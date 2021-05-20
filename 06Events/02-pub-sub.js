59d2d5b268b3c3906950bcb53226ef0ad012c986
“dongyuekai” <457545648@qq.com>
2021-05-20 14:50:39 +0800

    实现发布订阅

---
+++ 06Events/02-pub-sub.js
@@ -0,0 +1,34 @@
+// 实现发布订阅
+class PubSub {
+  constructor() {
+    this._events = {}
+  }
+  // 注册
+  subscribe(event, callback) {
+    if (this._events[event]) {
+      // 如果当前 event 存在，所以我们只需要往后添加当前次监听操作
+      this._events[event].push(callback)
+    } else {
+      // 之前没有订阅过此事件
+      this._events[event] = [callback]
+    }
+  }
+
+  // 发布
+  publish(event, ...args) {
+    const items = this._events[event]
+    if (items && items.length) {
+      items.forEach(callback => {
+        console.log('this---', this)
+        callback.call(this, ...args)
+      })
+    }
+  }
+}
+
+// 测试
+let ps = new PubSub()
+ps.subscribe('事件1', () => {
+  console.log('事件1执行了~~')
+})
+ps.publish('事件1')
