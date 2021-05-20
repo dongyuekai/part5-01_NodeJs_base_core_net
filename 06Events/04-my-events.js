4fe4ea8862286b053b2e5dcfb454d931b5f7e499
“dongyuekai” <457545648@qq.com>
2021-05-20 15:21:24 +0800

    模拟实现EventEmitter

---
+++ 06Events/04-my-events.js
@@ -0,0 +1,52 @@
+// 模拟EventEmitter
+
+function MyEvent() {
+  // 准备一个数据结构用于缓存订阅者信息
+  this._events = Object.create(null)
+}
+
+MyEvent.prototype.on = function (type, callback) {
+  // 判断当前次的事件是否已经存在，然后再决定如何做缓存
+  if (this._events[type]) {
+    this._events[type].push(callback)
+  } else {
+    this._events[type] = [callback]
+  }
+}
+MyEvent.prototype.emit = function (type, ...args) {
+  if (this._events && this._events[type].length) {
+    this._events[type].forEach((callback) => {
+      callback.call(this, ...args)
+    })
+  }
+}
+
+MyEvent.prototype.off = function (type, callback) {
+  // 判断当前 type 事件监听是否存在，如果存在则取消指定的监听
+  if (this._events && this._events[type]) {
+    this._events[type] = this._events[type].filter((item) => {
+      return item !== callback && item.link !== callback
+    })
+  }
+}
+MyEvent.prototype.once = function (type, callback) {
+  let foo = function (...args) {
+    callback.call(this, ...args)
+    this.off(type, foo)
+  }
+  foo.link = callback
+  this.on(type, foo)
+}
+
+
+let ev = new MyEvent()
+
+let fn = function (...data) {
+  console.log('事件1执行了', data)
+}
+
+ev.on('事件1', fn)
+ev.on('事件1', () => {
+  console.log('事件1----')
+})
+ev.emit('事件1', '参数1')
